7aaa1b12fda9b3da6364ed241790d3ef409b526e
lminasiewicz <l.minasiewicz.027@studms.ug.edu.pl>
2024-01-15 00:57:52 +0100

    feature @ backend: 2 new functions create_token and delete_token @ helper_functions.py for authentication

--- app/backend/helper_functions.py
+++ app/backend/helper_functions.py
@@ -7,3 +7,21 @@ def authenticate_token(tx, token: str, username: str) -> bool:
         return False
     else:
         return True
+
+
+def create_token(tx) -> str:
+    unique = False
+    while not unique:
+        query = "call apoc.create.uuids(1)"
+        results = tx.run(query).data()[0]["uuid"]
+        query2 = "match (a:Authentication) return a.token"
+        results2 = tx.run(query).data()
+        parsed_results2 = [result["a.token"] for result in results2]
+        if results not in parsed_results2:
+            unique = True
+    return results
+
+
+def delete_token(tx, token) -> None:
+    query = f"match (a:Authentication) where a.token = \"{token}\" delete a"
+    results = tx.run(query).data()
