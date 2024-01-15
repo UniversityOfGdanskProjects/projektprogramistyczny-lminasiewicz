fa65241462e303a1110736550735e147a84c17b7
lminasiewicz <l.minasiewicz.027@studms.ug.edu.pl>
2024-01-15 02:52:36 +0100

    feature @ backend: added password hashing functions to helper_functions, added post_user_login @ /api/users/login (POST)

--- app/backend/helper_functions.py
+++ app/backend/helper_functions.py
@@ -1,3 +1,5 @@
+from bcrypt import hashpw, gensalt, checkpw
+
 ### ------------ HELPER FUNCTIONS ------------- ###
 
 def authenticate_token(tx, token: str, username: str) -> bool:
@@ -25,3 +27,11 @@ def create_token(tx) -> str:
 def delete_token(tx, token) -> None:
     query = f"match (a:Authentication) where a.token = \"{token}\" delete a"
     results = tx.run(query).data()
+
+
+def hash_password(pwd: str) -> str:
+    return hashpw(pwd.encode("utf-8"), gensalt())
+
+
+def check_password(pwd: str, hashed: str) -> bool:
+    return checkpw(pwd.encode("utf-8"), hashed)
--- app/backend/user_routes/users_post.py
+++ app/backend/user_routes/users_post.py
@@ -8,3 +8,24 @@ from db_connection import driver
 from helper_functions import *
 
 bp_upost = Blueprint("bp_upost", __name__)
+
+
+def post_user_login(tx, login, pwd) -> dict|None:
+    query = f"match (u:User) where u.username = \"{login}\" return u.password"
+    hashed = tx.run(query).data()[0]["u.password"]
+    if check_password(pwd, hashed):
+        token = create_token(tx)
+        query2 = f"match (u:User) where u.username = \"{login}\" create (a:Authentication {{token: \"{token}\"}})-[:LOGS_IN]->(u)"
+        _ = tx.run(query2)
+        return {"username": login, "token": token}
+    
+
+
+@bp_upost.route("/api/users/login", methods=["POST"])
+def post_user_login_route():
+    req = request.json
+    login = req["login"]
+    pwd = req["password"]
+    session_data = driver.session().execute_read(post_user_login, login, pwd)
+    print("Received a POST request on endpoint /api/users/login")
+    return session_data, 200
